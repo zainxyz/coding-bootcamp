67cc762dc3c53cd56634ee5b5b3b49066c505243
Zain Abidi <hello@zainabidi.com>
2019-01-29 22:26:33 -0800

    test: reverse-string

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,8 @@
  * @type {Object}
  */
 const jestConfig = {
-    resolver                    : require.resolve('jest-pnp-resolver'),
-    setupTestFrameworkScriptFile: '<rootDir>/scripts/jest.js',
-    testEnvironment             : 'node',
-    testMatch                   : ['<rootDir>/**/__tests__/*.test.js']
+    testEnvironment: 'node',
+    testMatch      : ['<rootDir>/**/test.js']
 };
 
 module.exports = jestConfig;
---
+++ js/reverse-string/index.js
@@ -0,0 +1,30 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    return str.split('').reduce((res, char) => `${char}${res}`, '');
+}
+
+export default reverse;
+
+// function reverse(str) {
+//     return str
+//         .split('')
+//         .reverse()
+//         .join('');
+// }
+
+// function reverse(str) {
+//     let reversed = '';
+//
+//     for (let char of str) {
+//         reversed = `${char}${reversed}`;
+//     }
+//
+//     return reversed;
+// }
